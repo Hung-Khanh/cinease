ea6f18ae8c68336b0dbf4d6dd5b38b435dd1acfe
Hung-Khanh <hung147ty@gmail.com>
2025-07-28 14:29:25 +0700

    fix_name_folder

--- src/pages/Feedback/Feedback.jsx
+++ src/pages/Feedback/Feedback.jsx
@@ -3,7 +3,7 @@ import { List, Typography, Empty, Spin, message } from "antd"
 import { StarOutlined, PlusOutlined } from "@ant-design/icons"
 import { updateFeedback, deleteFeedback, getCurrentUserFeedbacks } from "../../api/feedback"
 import { FeedbackCard, FeedbackModal } from "./FeedbackComponents"
-import "./feedback.scss"
+import "./Feedbackkkk.scss"
 
 const { Title } = Typography
 
