aca747fbc4a2cd0eb4a74145e5112c9ebbe9fafe
Hung-Khanh <hung147ty@gmail.com>
2025-08-04 09:31:26 +0700

    deployv3

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import {
 import { AuthProvider } from "./constants/AuthProvider";
 import { store } from "./store/store";
 
-// ...existing code...
 import Footer from "./component/footer/Footer.jsx";
 import ForgotPassword from "./forgotPassword/forgotPassword.jsx";
 import Confirm from "./pages/confirm/Confirm.jsx";
@@ -35,23 +34,19 @@ import PhoneInput from "./pages/staff/jsx/inputPhoneNumber.jsx";
 import SeatSelection from "./pages/staff/jsx/seatSelection.jsx";
 import StaffHomePage from "./pages/staff/jsx/sHomePage.jsx";
 import TicketInformation from "./pages/staff/jsx/ticketInformation.jsx";
-// ...existing code...
+
 import backgroundImage from "./assets/bigbackground.png";
 import AdminHeader from "./component/admin/header/Header.jsx";
 import SideBar from "./component/admin/sideBar/sideBar.jsx";
 import ScrollToTop from "./component/scrollToTop.jsx";
-import CinemaRooms from "./pages/admin/cinemaRoom/cinemaRoom.jsx";
-import Dashboard from "./pages/admin/DashBoard/DashBoard.jsx";
-import Members from "./pages/admin/members/Members.jsx";
-import AdminMovies from "./pages/admin/Movies/Movie.jsx";
-import backgroundImage from "./assets/bigbackground.png";
-import Employees from "./pages/admin/Employees/Employees.jsx";
 import CinemaRooms from "./pages/admin/CinemaRoom/CinemaRoom.jsx";
+import Dashboard from "./pages/admin/DashBoard/DashBoard.jsx";
 import Members from "./pages/admin/Members/Members.jsx";
+import AdminMovies from "./pages/admin/Movies/Movie.jsx";
+import Employees from "./pages/admin/Employees/Employees.jsx";
 import TicketManagement from "./pages/admin/TicketManagement/TicketManagement.jsx";
 import ProductManagement from "./pages/admin/ProductManagement/ProductManagement.jsx";
 import FeedbackManagement from "./pages/admin/FeedbackManagement/FeedbackManagement.jsx";
-// import ErrorBoundary from "./components/ErrorBoundary";
 import ErrorPage from "./pages/Error/ErrorPage.jsx";
 
 import CinemaSeating from "./pages/Staff/JSX/TestSeatSelection.jsx";
